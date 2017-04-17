f3d2cc62bc4726c96ce3c1acd0a8b0fd4f997d2d
LynzZen <mathias.s.lynge@gmail.com>
2017-04-17 13:15:54 +0200

    Got the fragment to work for two of the buttons

--- app/src/main/java/app/p2/b226/aau/caffeinetrackerfinal/MainActivity.java
+++ app/src/main/java/app/p2/b226/aau/caffeinetrackerfinal/MainActivity.java
@@ -1,15 +1,39 @@
 package app.p2.b226.aau.caffeinetrackerfinal;
 
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    public void CreateFragmentSettings(View view) {
+        Fragment fragment;
+        fragment = new SettingsFragment();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frament_place, fragment);
+        ft.commit();
+    }
+
+    public void CreateFragmentVisualization(View view) {
+        Fragment fragment;
+        fragment = new VisualizationFragment();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frament_place, fragment);
+        ft.commit();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,10 +45,11 @@ public class MainActivity extends AppCompatActivity {
 
                     return true;
                 case R.id.navigation_dashboard:
-
+                    CreateFragmentVisualization(null);
                     return true;
-                case R.id.navigation_notifications:
 
+                case R.id.navigation_notifications:
+                    CreateFragmentSettings(null);
                     return true;
             }
             return false;
@@ -32,14 +57,19 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+
 }
+
+
+
+
